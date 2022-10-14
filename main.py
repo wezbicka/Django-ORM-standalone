b11e281540da3ce94b4581cb092e82af743e1689
Polina <75391420+wezbicka@users.noreply.github.com>
2022-10-14 14:59:24 +0300

    Filter the database

--- main.py
+++ main.py
@@ -9,12 +9,7 @@ from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
     # Программируем здесь
-    passcards = Passcard.objects.all()
-    active_passcards = []
-    for card in passcards:
-        if card.is_active:
-            active_passcards.append(card)
+    active_passcards = Passcard.objects.filter(is_active = True)
         
     print("Активных пропусков", len(active_passcards))
-    print(active_passcards)
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
