9f1bbbd482910748403f3fa04282c0ae4d0f0b51
Polina <75391420+wezbicka@users.noreply.github.com>
2022-10-14 15:54:30 +0300

    Fetched all users' visits

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
 from datacenter.models import Passcard  # noqa: E402
+from datacenter.models import Visit
 
 if __name__ == '__main__':
     # Программируем здесь
     active_passcards = Passcard.objects.filter(is_active = True)
-        
     print("Активных пропусков", len(active_passcards))
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    visits = Visit.objects.all() 
+    print(visits)
