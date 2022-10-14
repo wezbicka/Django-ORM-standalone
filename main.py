65023e6d835625bed470e4d6bd95b6d775157e7b
Polina <75391420+wezbicka@users.noreply.github.com>
2022-10-14 13:23:35 +0300

    Fetched all passcards

--- main.py
+++ main.py
@@ -9,4 +9,6 @@ from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
     # Программируем здесь
+    passcards = Passcard.objects.all()
+    print(passcards)
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
